6f9cf8087bd0b1f5ee948a3c6f325a9924820d21
CoolZonee <ckeat0203@gmail.com>
2021-11-09 00:45:45 +0800

    Update login api

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Login.module.css';
 
 export default function Login() {
+    const [username, setUsername] = useState<String>();
+    const [password, setPassword] = useState<String>();
+
+    async function loginUser(credentials) {
+        axois.get("http://127.0.0.1:8000/")
+    }
+
     return (
         <>
             <div className={styles.title}>
                 <h1>Login</h1>
             </div>
             <div className={styles.form_container}>
-                <form method="POST">
-
+                <form>
                     <div className={styles.input_container}>
                         <div>
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
                         </div>
                         <div>
-                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
                         </div>
                     </div>
                     <button type="submit">Sign In</button>
